97b7716360639bb5d8f19725074d2f06f28c3e5b
Fatkhulov Aynur <ainur345@yandex.ru>
2021-09-14 19:38:32 +0500

    Реализовал алгоритм преобразования строки в double

---
+++ TypeConverter/Converter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConverter
+{
+    /// <summary>
+    /// Содержит методы приведения типов.
+    /// </summary>
+    public class StringConverter
+    {
+        private Dictionary<int, char> DigitCharCompliance { get; set; }
+
+        public StringConverter()
+        {
+            DigitCharCompliance = new Dictionary<int, char>();
+            DigitCharCompliance.Add(0, '0');
+            DigitCharCompliance.Add(1, '1');
+            DigitCharCompliance.Add(2, '2');
+            DigitCharCompliance.Add(3, '3');
+            DigitCharCompliance.Add(4, '4');
+            DigitCharCompliance.Add(5, '5');
+            DigitCharCompliance.Add(6, '6');
+            DigitCharCompliance.Add(7, '7');
+            DigitCharCompliance.Add(8, '8');
+            DigitCharCompliance.Add(9, '9');
+        }
+
+        /// <summary>
+        /// Конвертирует строку в Double.
+        /// </summary>
+        /// <param name="number">Число в строковом представлении.</param>
+        /// <returns>Число конвертированное в Double.</returns>
+        public double ToDouble(string number)
+        {
+            List<char> charDigits = number.ToList();
+            List<double> digitsIntPart = new List<double>();
+            List<double> digitsFractionalPart = new List<double>();
+
+            bool hasMetPoint = false;
+            bool isKnownSymbol = false;
+            foreach (var digit in charDigits)
+            {
+                foreach (KeyValuePair<int, char> keyValuePair in DigitCharCompliance)
+                {
+                    if (digit == keyValuePair.Value && !hasMetPoint)
+                    {
+                        digitsIntPart.Add(keyValuePair.Key);
+                        isKnownSymbol = true;
+                        break;
+                    }
+                    if (digit == keyValuePair.Value && hasMetPoint)
+                    {
+                        digitsFractionalPart.Add(keyValuePair.Key);
+                        isKnownSymbol = true;
+                        break;
+                    }
+                    if (digit == '.')
+                    {
+                        if (hasMetPoint)
+                        {
+                            throw new FormatException("Входное чило вероятно имеет неверный формат. Проверьте вводимое число на корректность.");
+                        }
+                        hasMetPoint = true;
+                        isKnownSymbol = true;
+                        break;
+                    }
+                    isKnownSymbol = false;
+                }
+                if (!isKnownSymbol)
+                {
+                    throw new FormatException("Входное чило вероятно имеет неверный формат. Проверьте вводимое число на корректность.");
+                }
+            }
+
+            double numberIntPart = MakeSingleNumberFromList(digitsIntPart);
+            double numberFractionalPart = MakeSingleNumberFromList(digitsFractionalPart);
+            numberFractionalPart /= Math.Pow(10, digitsFractionalPart.Count); 
+
+            return numberIntPart + numberFractionalPart;
+        }
+
+        private static double MakeSingleNumberFromList(List<double> digitsIntPart)
+        {
+            double totalNumber = 0;
+            int count = digitsIntPart.Count - 1;
+            foreach (var digit in digitsIntPart)
+            {
+                var intPart = digit * Math.Pow(10, count);
+                count--;
+                totalNumber += intPart;
+            }
+
+            return totalNumber;
+        }
+    }
+}
--- TypeConverter/Program.cs
+++ TypeConverter/Program.cs
@@ -6,7 +6,39 @@ namespace TypeConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Результат преобразования строки в double: {SafelyConverToDouble()}");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Выполнить преобразование с обработкой ошибок ввода числа.
+        /// </summary>
+        /// <returns>Полученное число.</returns>
+        public static double SafelyConverToDouble()
+        {
+            var converter = new StringConverter();
+            double result = 0;
+            bool isOk = false;
+            Console.WriteLine("Введите число. Дробную часть необходимо указывать через точку (например: 83.2):");
+            while (!isOk)
+            {
+                try
+                {
+                    string number = Console.ReadLine();
+                    if (string.IsNullOrEmpty(number))
+                    {
+                        continue;
+                    }
+                    result = converter.ToDouble(number);
+                    isOk = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Произошла ошибка при обработке введеной строки, повторите ввод.");
+                    isOk = false;
+                }
+            }
+            return result;
         }
     }
 }
